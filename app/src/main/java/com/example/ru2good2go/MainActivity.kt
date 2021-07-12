71508c3b6852e41762a5481a24a973bbfd9f837c
Sarah McNey <sarahmcney@gmail.com>
2021-07-12 14:18:45 -0400

    Added button

--- app/src/main/java/com/example/ru2good2go/MainActivity.kt
+++ app/src/main/java/com/example/ru2good2go/MainActivity.kt
@@ -2,11 +2,22 @@ package com.example.ru2good2go
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val text = findViewById<TextView>(R.id.text)
+        text.visibility = View.INVISIBLE
+
+        val button = findViewById<Button>(R.id.button)
+        button.setOnClickListener {
+            text.visibility = View.VISIBLE
+        }
     }
 }
